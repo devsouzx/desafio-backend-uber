bb107278dd6a62b1dec80ac64cd1f82a97090f5c
João Emanuel <joaoemanuelmarinhosousa@gmail.com>
2024-09-28 20:14:19 -0300

    creating application layer

---
+++ src/main/java/com/devsouzx/email_service/apllication/EmailSenderService.java
@@ -0,0 +1,21 @@
+package com.devsouzx.email_service.apllication;
+
+import com.devsouzx.email_service.adapters.EmailSenderGateway;
+import com.devsouzx.email_service.core.EmailSenderUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderService implements EmailSenderUseCase {
+    private EmailSenderGateway emailSenderGateway;
+
+    @Autowired
+    public EmailSenderService(EmailSenderGateway emailSenderGateway) {
+        this.emailSenderGateway = emailSenderGateway;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateway.sendEmail(to, subject, body);
+    }
+}
